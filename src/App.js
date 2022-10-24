043f45e38749add7eb862a753c40c905cd282272
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-24 13:42:41 +0000

    fix the app and make components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { Box } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import './App.css'
+import Footer from './components/Footer';
+
+import Navbar from './components/Navbar'
+import ExerciseDetail from './pages/ExerciseDetail';
+
+import Home from './pages/Home';
+
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   );
 };
 
