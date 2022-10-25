1fdde52b16248078ea3ae53384c78cd0eed2f29a
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-25 11:19:21 +0000

    Footer :D

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import { Box } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Footer from './components/Footer';
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Navbar from './components/Navbar'
-import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
-import Home from './pages/Home';
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
 
-const App = () => {
-  return (
-    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetail />} />
-      </Routes>
-      <Footer />
-    </Box>
-  );
-};
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
 export default App;
