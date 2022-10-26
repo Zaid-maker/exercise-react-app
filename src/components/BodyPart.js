23cd29ae054311b0cfaf6f2e8ec63e0fcd1f2f47
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-26 17:39:00 +0000

    fix some stuffs and ix search exercise

---
+++ src/components/BodyPart.js
@@ -0,0 +1,10 @@
+import { Stack, Typography } from '@mui/material'
+import React from 'react'
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+  <Stack>
+    <Typography> {item}</Typography>
+  </Stack>
+}
+
+export default BodyPart
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,9 +1,53 @@
+/* eslint-disable no-unused-vars */
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
   const [bodyParts, setBodyParts] = useState([]);
+
+  /* Fetching the data from the API and setting the state of the bodyParts to the fetched data. */
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts(["all", ...bodyPartsData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  /**
+   * It takes the search input from the user, fetches the data from the API, filters the data based on
+   * the search input, and then sets the state of the exercises to the filtered data
+   */
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
@@ -25,6 +69,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
             borderRadius: "40px",
           }}
           value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase())}
           placeholder="Search Exercises"
           type="text"
         />
@@ -40,9 +85,18 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
             right: "0px",
             fontSize: { lg: "20px", xs: "14px" },
           }}
+          onClick={handleSearch}
         >
           Search
         </Button>
+      </Box>
+      <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyParts
+          setBodyPart={setBodyPart}
+          bodyPart={bodyPart}
+        />
       </Box>
     </Stack>
   );
