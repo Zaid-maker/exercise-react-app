7a87445922eb44e5b70e20bb6d664d088bc490fa
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-10-28 10:40:50 +0000

    Exercise Videos

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -42,7 +42,7 @@ const ExerciseDetail = () => {
   return (
     <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
